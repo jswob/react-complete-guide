4440457b692cc66c2e38e30c8de0a33fe137405e
jswob <jacswob@gmail.com>
2020-12-17 15:08:47 +0100

    Use prop types to secure Person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,33 @@
-import React from "react";
-
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import "./Person.css";
+import withClass from "../../../hoc/withClass";
 
-const person = (props) => {
-  return (
-    <div className="Person">
-      <p onClick={props.click}>
-        I'm a {props.name} and I am {props.age} yaers old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} defaultValue={props.name} />
-    </div>
-  );
+class Person extends Component {
+  render() {
+    console.log("   [Person.js] render");
+
+    return (
+      <Fragment>
+        <p onClick={this.props.click}>
+          I'm a {this.props.name} and I am {this.props.age} yaers old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          defaultValue={this.props.name}
+        />
+      </Fragment>
+    );
+  }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
 };
 
-export default person;
+export default withClass(Person, "Person");
